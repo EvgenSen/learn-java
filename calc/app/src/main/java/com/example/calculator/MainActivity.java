a515a030f01f23380cfcf10254372cdaa48ac40e
Evgeniy <sennikov-work@ya.ru>
2023-03-18 14:05:10 +0700

    calc: add new buttons, and top text banner, check 0 division, fix some bugs

--- calc/app/src/main/java/com/example/calculator/MainActivity.java
+++ calc/app/src/main/java/com/example/calculator/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.graphics.Color;
+import android.content.res.ColorStateList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,68 +21,122 @@ public class MainActivity extends AppCompatActivity {
     ActionList curAction = ActionList.NOPE;
     Double firstNumber = 0.0;
 
+    private void addCharToScreen(String Ch) {
+        TextView outText = (TextView) findViewById(R.id.outputText );
+        String str = String.valueOf((String) outText.getText());
+
+        switch (Ch) {
+            case ".":
+                if(str.indexOf(".") > 0) {
+                    return;
+                }
+                if(str.length() == 0) {
+                    outText.setText("0");
+                }
+                break;
+            case "0":
+                if(str.length() == 1 && str.charAt(0) == '0') {
+                    return;
+                }
+                break;
+            default:
+                break;
+        }
+
+        outText.setText(outText.getText() + Ch);
+        // outText.append(Ch); // TODO
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView outText = (TextView) findViewById(R.id.outputText );
+        TextView outText = (TextView) findViewById(R.id.outputText);
+        TextView topText = (TextView) findViewById(R.id.topText);
+        ColorStateList defColors = topText.getTextColors();
 
         Button b1 = (Button) findViewById(R.id.buttonNum1);
         b1.setOnClickListener(view -> {
-            outText.setText(outText.getText() + "1");
+            this.addCharToScreen("1");
         });
 
         Button b2 = (Button) findViewById(R.id.buttonNum2);
         b2.setOnClickListener(view -> {
-            outText.setText(outText.getText() + "2");
+            this.addCharToScreen("2");
         });
 
         Button b3 = (Button) findViewById(R.id.buttonNum3);
         b3.setOnClickListener(view -> {
-            outText.setText(outText.getText() + "3");
+            this.addCharToScreen("3");
         });
 
         Button b4 = (Button) findViewById(R.id.buttonNum4);
         b4.setOnClickListener(view -> {
-            outText.setText(outText.getText() + "4");
+            this.addCharToScreen("4");
         });
 
         Button b5 = (Button) findViewById(R.id.buttonNum5);
         b5.setOnClickListener(view -> {
-            outText.setText(outText.getText() + "5");
+            this.addCharToScreen("5");
         });
 
         Button b6 = (Button) findViewById(R.id.buttonNum6);
         b6.setOnClickListener(view -> {
-            outText.setText(outText.getText() + "6");
+            this.addCharToScreen("6");
         });
 
         Button b7 = (Button) findViewById(R.id.buttonNum7);
         b7.setOnClickListener(view -> {
-            outText.setText(outText.getText() + "7");
+            this.addCharToScreen("7");
         });
 
         Button b8 = (Button) findViewById(R.id.buttonNum8);
         b8.setOnClickListener(view -> {
-            outText.setText(outText.getText() + "8");
+            this.addCharToScreen("8");
         });
 
         Button b9 = (Button) findViewById(R.id.buttonNum9);
         b9.setOnClickListener(view -> {
-            outText.setText(outText.getText() + "9");
+            this.addCharToScreen("9");
         });
 
         Button b0 = (Button) findViewById(R.id.buttonNum0);
         b0.setOnClickListener(view -> {
-            outText.setText(outText.getText() + "0");
+            this.addCharToScreen("0");
         });
 
-        Button actRM = (Button) findViewById(R.id.buttonActRM);
-        actRM.setOnClickListener(view -> {
+        Button actDot = (Button) findViewById(R.id.buttonActDot);
+        actDot.setOnClickListener(view -> {
+            this.addCharToScreen(".");
+        });
+
+        Button actClean = (Button) findViewById(R.id.buttonActClean);
+        actClean.setOnClickListener(view -> {
             String str = String.valueOf((String) outText.getText());
             if (str != null && str.length() > 0) {
                 outText.setText(str.substring(0, str.length() - 1));
+            }
+        });
+
+        Button actCleanAll = (Button) findViewById(R.id.buttonActCleanAll);
+        actCleanAll.setOnClickListener(view -> {
+            outText.setText("");
+            topText.setText("");
+            topText.setTextColor(defColors);
+            firstNumber = 0.0;
+            curAction = ActionList.NOPE;
+        });
+
+        Button actPercent = (Button) findViewById(R.id.buttonActPercent);
+        actPercent.setOnClickListener(view -> {
+            String str = String.valueOf((String) outText.getText());
+            if (str != null && str.length() > 0) {
+                curAction = ActionList.NOPE;
+                firstNumber = Double.valueOf(str) / 100.0;
+                topText.setText(str + "% = ");
+                topText.setTextColor(defColors);
+                outText.setText(String.valueOf(firstNumber));
             }
         });
 
@@ -90,6 +146,8 @@ public class MainActivity extends AppCompatActivity {
             if (str != null && str.length() > 0) {
                 curAction = ActionList.PLUS;
                 firstNumber = Double.valueOf(str);
+                topText.setText(str + " + ");
+                topText.setTextColor(defColors);
                 outText.setText("");
             }
         });
@@ -100,6 +158,8 @@ public class MainActivity extends AppCompatActivity {
             if (str != null && str.length() > 0) {
                 curAction = ActionList.MINUS;
                 firstNumber = Double.valueOf(str);
+                topText.setText(str + " - ");
+                topText.setTextColor(defColors);
                 outText.setText("");
             }
         });
@@ -110,6 +170,8 @@ public class MainActivity extends AppCompatActivity {
             if (str != null && str.length() > 0) {
                 curAction = ActionList.MULTI;
                 firstNumber = Double.valueOf(str);
+                topText.setText(str + " * ");
+                topText.setTextColor(defColors);
                 outText.setText("");
             }
         });
@@ -120,6 +182,8 @@ public class MainActivity extends AppCompatActivity {
             if (str != null && str.length() > 0) {
                 curAction = ActionList.DIV;
                 firstNumber = Double.valueOf(str);
+                topText.setText(str + " / ");
+                topText.setTextColor(defColors);
                 outText.setText("");
             }
         });
@@ -138,18 +202,34 @@ public class MainActivity extends AppCompatActivity {
             switch (curAction) {
                 case PLUS:
                     firstNumber = firstNumber + secondValue;
+                    topText.setText((String)topText.getText() + String.valueOf(secondValue) + " =");
+                    topText.setTextColor(defColors);
                     outText.setText(String.valueOf(firstNumber));
                     break;
                 case MINUS:
                     firstNumber = firstNumber - secondValue;
+                    topText.setText((String)topText.getText() + String.valueOf(secondValue) + " =");
+                    topText.setTextColor(defColors);
                     outText.setText(String.valueOf(firstNumber));
                     break;
                 case MULTI:
                     firstNumber = firstNumber * secondValue;
+                    topText.setText((String)topText.getText() + String.valueOf(secondValue) + " =");
+                    topText.setTextColor(defColors);
                     outText.setText(String.valueOf(firstNumber));
                     break;
                 case DIV:
-                    firstNumber = firstNumber / secondValue;
+                    if(secondValue == 0.0) {
+                        topText.setTextColor(Color.RED);
+                        topText.setText("Error: division by 0");
+                        firstNumber = 0.0;
+                    }
+                    else {
+                        topText.setText((String)topText.getText() + String.valueOf(secondValue) + " =");
+                        topText.setTextColor(defColors);
+                        firstNumber = firstNumber / secondValue;
+                    }
+
                     outText.setText(String.valueOf(firstNumber));
                     break;
                 default:
